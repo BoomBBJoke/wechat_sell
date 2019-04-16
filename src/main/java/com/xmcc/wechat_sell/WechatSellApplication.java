d4afd51038c0cc71cce9681040ba9c9b398e7161
BoomBBJoke <17783296200@163.com>
2019-04-16 15:48:41 +0800

    第一次提交：项目空壳

---
+++ src/main/java/com/xmcc/wechat_sell/WechatSellApplication.java
@@ -0,0 +1,13 @@
+package com.xmcc.wechat_sell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatSellApplication.class, args);
+	}
+
+}
